25c98b2ff754681c28d8542efb90e5ec136ce678
Praxis <164508654+prraas@users.noreply.github.com>
2026-02-13 16:50:22 +0000

    feat(sdk): export SkillsAPI in package __all__

--- praxis/__init__.py
+++ praxis/__init__.py
@@ -1,5 +1,6 @@
 from praxis.client import Client
 from praxis.session import Session
 from praxis.version import __version__
+from praxis.api.skills import SkillsAPI
 
-__all__ = ["Client", "Session", "__version__"]
+__all__ = ["Client", "Session","SkillsAPI", "__version__"]
