023936bfcd6072c32925c37550ec13536a5e0ea9
Praxis <164508654+prraas@users.noreply.github.com>
2026-02-24 14:02:19 +0530

    refactor(sdk): mount newly engineered assembly/analytics to index exports

--- praxis/__init__.py
+++ praxis/__init__.py
@@ -3,5 +3,6 @@ from praxis.session import Session
 from praxis.version import __version__
 from praxis.api.manipulation import ManipulationAPI
 from praxis.api.sorting import SortingAPI
+from praxis.api.analytics import AnalyticsAPI
 
-__all__ = ["Client", "Session", "ManipulationAPI", "SortingAPI", "__version__"]
+__all__ = ["Client", "Session", "ManipulationAPI", "SortingAPI", "AnalyticsAPI", "__version__"]
