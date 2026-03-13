4da5bb14be7bc01b43e30b54973746bc25c23556
Praxis <164508654+prraas@users.noreply.github.com>
2026-03-13 09:55:16 +0000

    feat(core): export all domain APIs and spatial models in package root

--- praxis/__init__.py
+++ praxis/__init__.py
@@ -1,8 +1,30 @@
 from praxis.client import Client
 from praxis.session import Session
 from praxis.version import __version__
+from praxis.api.physics import PhysicsAPI
+from praxis.api.navigation import NavigationAPI
+from praxis.api.simulation import SimulationAPI
+from praxis.api.vision import VisionAPI
 from praxis.api.manipulation import ManipulationAPI
 from praxis.api.sorting import SortingAPI
 from praxis.api.analytics import AnalyticsAPI
+from praxis.api.assembly import AssemblyAPI
+from praxis.models.spatial import SegmentedObject, SegmentationResult
+from praxis.core import spatial_utils
 
-__all__ = ["Client", "Session", "ManipulationAPI", "SortingAPI", "AnalyticsAPI", "__version__"]
+__all__ = [
+    "Client",
+    "Session",
+    "PhysicsAPI",
+    "NavigationAPI",
+    "SimulationAPI",
+    "VisionAPI",
+    "ManipulationAPI",
+    "SortingAPI",
+    "AnalyticsAPI",
+    "AssemblyAPI",
+    "SegmentedObject",
+    "SegmentationResult",
+    "spatial_utils",
+    "__version__"
+]
